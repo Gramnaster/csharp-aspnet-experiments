bbd41e657fcc073b4a1e2c990f7ff623460a4e6d
Gramnaster <jocvillalon@yahoo.com.ph>
2026-02-23 22:30:49 +0800

    DI: Adds CitiesService to HomeController

---
+++ 12-DIExample-Services/CitiesService.cs
@@ -0,0 +1,24 @@
+﻿namespace _12_DIExample_Services
+{
+    public class CitiesService
+    {
+        // Anything logic not related to the Controller or DB layer
+        private List<string> _cities = [];
+
+        public CitiesService()
+        {
+            _cities = [
+                "London", 
+                "Paris", 
+                "New York", 
+                "Tokyo", 
+                "Rome"
+            ];
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    }
+}
---
+++ 12-DIExample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using _12_DIExample_Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _12_DIExample.Controllers
+{
+    [ApiController]
+public class HomeController : Controller
+    {
+        private readonly CitiesService _citiesService;
+
+        public HomeController()
+        {
+            _citiesService = new CitiesService();
+        }
+
+        [Route("/")]    
+        public IActionResult Index()
+        {
+            List<string> cities = _citiesService.GetCities();
+            return Ok(cities);
+        }
+    }
+}
